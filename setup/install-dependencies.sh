f10a125b58d379cb5d68d4048c5825894d0bb0f9
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-02-26 23:42:57 -0500

    Swap kubectl for curl install

--- setup/install-dependencies.sh
+++ setup/install-dependencies.sh
@@ -15,7 +15,10 @@ echo "Installing docker"
 sudo snap install docker
 
 echo "Installing kubectl"
-sudo snap install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(<kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 echo "Installing k3d"
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
