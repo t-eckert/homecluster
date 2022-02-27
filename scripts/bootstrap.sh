bb47969c710e1f11877ad30ed82911858872b9d0
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-02-26 22:13:54 -0500

    Fix kubectl install

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -22,6 +22,9 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 echo "Installing kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(<kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 echo "Installing k3d (the K3s command line tool)"
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
