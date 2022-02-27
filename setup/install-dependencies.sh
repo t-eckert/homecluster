34bc732712a687333dd3423dc2cf20a0bb3a4c99
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-02-26 23:40:25 -0500

    Add kubectl to dependencies

--- setup/install-dependencies.sh
+++ setup/install-dependencies.sh
@@ -2,10 +2,20 @@
 
 echo "Installing dependencies"
 cat << EOM
-This script uses snapd to install the following packages:
+This script installs the following packages:
   - docker
   - kubectl
+  - k3d
 EOM
 
+echo "Installing snapd core"
 sudo snap install core
+
+echo "Installing docker"
 sudo snap install docker
+
+echo "Installing kubectl"
+sudo snap install kubectl
+
+echo "Installing k3d"
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
