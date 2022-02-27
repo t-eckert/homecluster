d2cf1fd1c0196c11e36c02dd1567c2ce99f7f12a
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-02-26 22:05:28 -0500

    Add bootstrap script

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Starting Bootstrap, this script will install Docker daemon and set up a " \
+"K3s cluster using the k3d tool."
+
+echo "Removing any existing Docker daemons"
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+echo "Installing Docker"
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+echo "Installing k3d (the K3s command line tool)"
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
