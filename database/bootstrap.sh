7ea4a230ee9b0aeb32acdc44f791d0fecc2c1b89
Thomas Eckert <thomas.james.eckert@gmail.com>
2023-08-30 17:59:51 -0400

    Add docker-compose to installs

--- database/bootstrap.sh
+++ database/bootstrap.sh
@@ -16,4 +16,4 @@ echo \
 
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
